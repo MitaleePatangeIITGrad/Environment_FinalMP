dffea5d6ff2a04f220afaeddc0e46654246aa73c
MitPat <mpatange@hawk.iit.edu>
2015-11-23 16:57:10 -0600

    Changes to point paths to final project folders

--- install-env.sh
+++ install-env.sh
@@ -2,21 +2,23 @@
 
 sudo apt-get -y update
 
-sudo apt-get -y install apache2 git php5 php5-curl mysql-client curl php5-mysql
+sudo apt-get -y install apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick imagemagick
+sudo phpp5enmod imagick
+sudo service apache2 reload
 
 git clone https://github.com/MitPat/Application_MP2.git
 
-mv ./Application_MP2/images /var/www/html/images
+mv ./Application_FinalMP/images /var/www/html/images
 
-mv ./Application_MP2/*.html /var/www/html
+mv ./Application_FinalMP/*.html /var/www/html
 
-mv ./Application_MP2/*.php /var/www/html
+mv ./Application_FinalMP/*.php /var/www/html
 
-mv ./Application_MP2/css /var/www/html/css
+mv ./Application_FinalMP/css /var/www/html/css
 
-mv ./Application_MP2/js /var/www/html/js
+mv ./Application_FinalMP/js /var/www/html/js
 
-mv ./Application_MP2/img /var/www/html/img
+mv ./Application_FinalMP/img /var/www/html/img
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
